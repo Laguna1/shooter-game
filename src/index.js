720e16c89c5db49b5998b69732e83055753c76ea
Laguna1 <lagunazoomarket@gmail.com>
2020-12-30 18:25:29 +0200

    Preparing to compile and download content

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import SceneMainMenu from "./SceneMainMenu";
+import SceneMain from "./SceneMain";
+import SceneGameOver from "./SceneGameOver";
+
+var config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  backgroundColor: "black",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  scene: [SceneMainMenu, SceneMain, SceneGameOver],
+  pixelArt: true,
+  roundPixels: true,
+};
+
+var game = new Phaser.Game(config);

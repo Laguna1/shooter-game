25e1aa9a838f634e11951d22205c7d9264ce17ad
Laguna1 <lagunazoomarket@gmail.com>
2021-01-04 01:10:40 +0200

    Add physics to player and enemies

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -1,5 +1,9 @@
 import Phaser from 'phaser';
 import Player from '../objects/player';
+import ScrollingBackground from './background';
+import GunShip from '../objects/gunship';
+import ChaserShip from '../objects/chaserShip';
+import CarrierShip from '../objects/carrierShip';
 
 export default class SceneMain extends Phaser.Scene {
   constructor() {
@@ -7,31 +11,31 @@ export default class SceneMain extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('sprBg0', '..src/assets/sprBg0.png');
-    this.load.image('sprBg1', '..src/assets/sprBg1.png');
-    this.load.spritesheet('sprExplosion', '..src/assets/sprExplosion.png', {
+    this.load.image('sprBg0', '../src/assets/sprBg0.png');
+    this.load.image('sprBg1', '../src/assets/sprBg1.png');
+    this.load.spritesheet('sprExplosion', '../src/assets/sprExplosion.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
 
-    this.load.spritesheet('sprEnemy0', '..src/assets/sprEnemy0.png', {
+    this.load.spritesheet('sprEnemy0', '../src/assets/sprEnemy0.png', {
       frameWidth: 16,
       frameHeight: 16,
     });
-    this.load.image('sprEnemy1', '..src/assets/sprEnemy1.png');
-    this.load.spritesheet('sprEnemy2', '..src/assets/sprEnemy2.png', {
+    this.load.image('sprEnemy1', '../src/assets/sprEnemy1.png');
+    this.load.spritesheet('sprEnemy2', '../src/assets/sprEnemy2.png', {
       frameWidth: 16,
       frameHeight: 16,
     });
-    this.load.image('sprLaserEnemy0', '..src/assets/sprLaserEnemy0.png');
-    this.load.image('sprLaserPlayer', '..src/assets/sprLaserPlayer.png');
-    this.load.spritesheet('sprPlayer', '..src/assets/sprPlayer.png', {
+    this.load.image('sprLaserEnemy0', '../src/assets/sprLaserEnemy0.png');
+    this.load.image('sprLaserPlayer', '../src/assets/sprLaserPlayer.png');
+    this.load.spritesheet('sprPlayer', '../src/assets/sprPlayer.png', {
       frameWidth: 16,
       frameHeight: 16,
     });
-    this.load.audio('sndExplode0', '..src/assets/sndExplode0.wav');
-    this.load.audio('sndExplode1', '..src/assets/sndExplode1.wav');
-    this.load.audio('sndLaser', '..src/assets/sndLaser.wav');
+    this.load.audio('sndExplode0', '../src/assets/sndExplode0.wav');
+    this.load.audio('sndExplode1', '../src/assets/sndExplode1.wav');
+    this.load.audio('sndLaser', '../src/assets/sndLaser.wav');
   }
 
   create() {
@@ -77,5 +81,180 @@ export default class SceneMain extends Phaser.Scene {
       ],
       laser: this.sound.add('sndLaser'),
     };
+    this.backgrounds = [];
+    for (let i = 0; i < 5; i += 1) { // create five scrolling backgrounds
+      const bg = new ScrollingBackground(this, 'sprBg0', i * 10);
+      this.backgrounds.push(bg);
+    }
+
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+
+    this.time.addEvent({
+      delay: 1000,
+      callback() {
+        let enemy = null;
+
+        if (Phaser.Math.Between(0, 10) >= 3) {
+          enemy = new GunShip(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0,
+          );
+        } else if (Phaser.Math.Between(0, 10) >= 5) {
+          if (this.getEnemiesByType('ChaserShip').length < 5) {
+            enemy = new ChaserShip(
+              this,
+              Phaser.Math.Between(0, this.game.config.width),
+              0,
+            );
+          }
+        } else {
+          enemy = new CarrierShip(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0,
+          );
+        }
+
+        if (enemy !== null) {
+          enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
+          this.enemies.add(enemy);
+        }
+      },
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.physics.add.overlap(this.player, this.enemyLasers, (player, laser) => {
+      if (!player.getData('isDead')
+        && !laser.getData('isDead')) {
+        player.explode(false);
+        player.onDestroy();
+        laser.destroy();
+      }
+    });
+    this.physics.add.collider(this.playerLasers, this.enemies, (playerLaser, enemy) => {
+      if (enemy) {
+        if (enemy.onDestroy !== undefined) {
+          enemy.onDestroy();
+        }
+
+        enemy.explode(true);
+        playerLaser.destroy();
+      }
+    });
+
+    this.physics.add.overlap(this.player, this.enemies, (player, enemy) => {
+      if (!player.getData('isDead')
+        && !enemy.getData('isDead')) {
+        player.explode(false);
+        player.onDestroy();
+        enemy.explode(true);
+      }
+    });
+
+    this.physics.add.overlap(this.player, this.enemyLasers, (player, laser) => {
+      if (!player.getData('isDead')
+        && !laser.getData('isDead')) {
+        player.explode(false);
+        player.onDestroy();
+        laser.destroy();
+      }
+    });
+  }
+
+  update() {
+    if (!this.player.getData('isDead')) {
+      this.player.update();
+
+      if (this.keyW.isDown) {
+        this.player.moveUp();
+      } else if (this.keyS.isDown) {
+        this.player.moveDown();
+      }
+
+      if (this.keyA.isDown) {
+        this.player.moveLeft();
+      } else if (this.keyD.isDown) {
+        this.player.moveRight();
+      }
+
+      if (this.keySpace.isDown) {
+        this.player.setData('isShooting', true);
+      } else {
+        this.player.setData('timerShootTick', this.player.getData('timerShootDelay') - 1);
+        this.player.setData('isShooting', false);
+      }
+    }
+
+
+    for (let i = 0; i < this.enemies.getChildren().length; i += 1) {
+      const enemy = this.enemies.getChildren()[i];
+
+      enemy.update();
+      if (enemy.x < -enemy.displayWidth
+      || enemy.x > this.game.config.width + enemy.displayWidth
+      || enemy.y < -enemy.displayHeight * 4
+      || enemy.y > this.game.config.height + enemy.displayHeight) {
+        if (enemy) {
+          if (enemy.onDestroy !== undefined) {
+            enemy.onDestroy();
+          }
+
+          enemy.destroy();
+        }
+      }
+    }
+
+    for (let i = 0; i < this.enemyLasers.getChildren().length; i += 1) {
+      const laser = this.enemyLasers.getChildren()[i];
+      laser.update();
+
+      if (laser.x < -laser.displayWidth
+      || laser.x > this.game.config.width + laser.displayWidth
+      || laser.y < -laser.displayHeight * 4
+      || laser.y > this.game.config.height + laser.displayHeight) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
+    }
+
+    for (let i = 0; i < this.playerLasers.getChildren().length; i += 1) {
+      const laser = this.playerLasers.getChildren()[i];
+      laser.update();
+
+      if (laser.x < -laser.displayWidth
+      || laser.x > this.game.config.width + laser.displayWidth
+      || laser.y < -laser.displayHeight * 4
+      || laser.y > this.game.config.height + laser.displayHeight) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
+    }
+
+    for (let i = 0; i < this.backgrounds.length; i += 1) {
+      this.backgrounds[i].update();
+    }
+  }
+
+  getEnemiesByType(type) {
+    const arr = [];
+    for (let i = 0; i < this.enemies.getChildren().length; i += 1) {
+      const enemy = this.enemies.getChildren()[i];
+      if (enemy.getData('type') === type) {
+        arr.push(enemy);
+      }
+    }
+    return arr;
   }
 }
--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import ScrollingBackground from './background';
 
 export default class SceneMainMenu extends Phaser.Scene {
   constructor() {
@@ -6,20 +7,74 @@ export default class SceneMainMenu extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('sprBg0', 'assets/sprBg0.png');
-    this.load.image('sprBg1', '..src/assets/sprBg1.png');
-    this.load.image('sprBtnPlay', '..src/assets/sprBtnPlay.png');
-    this.load.image('sprBtnPlayHover', '..src/assets/sprBtnPlayHover.png');
-    this.load.image('sprBtnPlayDown', '..src/assets/sprBtnPlayDown.png');
-    this.load.image('sprBtnRestart', '..src/assets/sprBtnRestart.png');
-    this.load.image('sprBtnRestartHover', '..src/assets/sprBtnRestartHover.png');
-    this.load.image('sprBtnRestartDown', '..src/assets/sprBtnRestartDown.png');
+    this.load.image('sprBg0', '../src/assets/sprBg0.png');
+    this.load.image('sprBg1', '../src/assets/sprBg1.png');
+    this.load.image('sprBtnPlay', '../src/assets/sprBtnPlay.png');
+    this.load.image('sprBtnPlayHover', '../src/assets/sprBtnPlayHover.png');
+    this.load.image('sprBtnPlayDown', '../src/assets/sprBtnPlayDown.png');
+    this.load.image('sprBtnRestart', '../src/assets/sprBtnRestart.png');
+    this.load.image('sprBtnRestartHover', '../src/assets/sprBtnRestartHover.png');
+    this.load.image('sprBtnRestartDown', '../src/assets/sprBtnRestartDown.png');
 
-    this.load.audio('sndBtnOver', '..src/assets/sndBtnOver.wav');
-    this.load.audio('sndBtnDown', '..src/assets/sndBtnDown.wav');
+    this.load.audio('sndBtnOver', '../src/assets/sndBtnOver.wav');
+    this.load.audio('sndBtnDown', '../src/assets/sndBtnDown.wav');
   }
 
   create() {
-    this.scene.start('SceneMain');
+    this.sfx = {
+      btnOver: this.sound.add('sndBtnOver'),
+      btnDown: this.sound.add('sndBtnDown'),
+    };
+
+    this.btnPlay = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      'sprBtnPlay',
+    );
+
+    this.btnPlay.setInteractive();
+
+    this.btnPlay.on('pointerover', () => {
+      this.btnPlay.setTexture('sprBtnPlayHover'); // set the button texture to sprBtnPlayHover
+      this.sfx.btnOver.play(); // play the button over sound
+    }, this);
+
+    this.btnPlay.on('pointerout', () => {
+      this.setTexture('sprBtnPlay');
+    });
+
+    this.btnPlay.on('pointerdown', () => {
+      this.btnPlay.setTexture('sprBtnPlayDown');
+      this.sfx.btnDown.play();
+    }, this);
+
+    this.btnPlay.on('pointerup', () => {
+      this.btnPlay.setTexture('sprBtnPlay');
+      this.scene.start('GameMain');
+    }, this);
+
+    this.title = this.add.text(this.game.config.width * 0.5, 128, 'SPACE SHOOTER', {
+      fontFamily: 'monospace',
+      fontSize: 48,
+      fontStyle: 'bold',
+      color: '#ffffff',
+      align: 'center',
+    });
+
+    this.title.setOrigin(0.5);
+
+    this.backgrounds = [];
+    for (let i = 0; i < 5; i += 1) {
+      const keys = ['sprBg0', 'sprBg1'];
+      const key = keys[Phaser.Math.Between(0, keys.length - 1)];
+      const bg = new ScrollingBackground(this, key, i * 10);
+      this.backgrounds.push(bg);
+    }
+  }
+
+  update() {
+    for (let i = 0; i < this.backgrounds.length; i += 1) {
+      this.backgrounds[i].update();
+    }
   }
 }
